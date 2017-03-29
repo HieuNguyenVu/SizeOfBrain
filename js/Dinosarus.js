f673064572f9698143783226923cf86e6182f484
HieuNguyenVu <nhvu.95@gmail.com>
2017-03-29 21:39:11 +0700

    conkhunglong

--- js/Dinosarus.js
+++ js/Dinosarus.js
@@ -4,11 +4,10 @@ class Dinosarus{
       this.sprite = Nakama.playerGroup.add(sprite);
       Nakama.game.physics.arcade.enable(this.sprite);
       this.configs = configs;
-      this.timeSinceLastFire = 0;
       this.sprite.anchor = new Phaser.Point(0.5,0.5);
       this.sprite.animations.add('run');
       //this about gravity
-      this.sprite.body.collideWorldBounds = true;
+      this.sprite.body.collideWorldBounds = false;
       this.sprite.body.bounce.y = 0.8;
       //out bound kill
       this.sprite.body.checkWarBounds = true;
@@ -18,7 +17,7 @@ class Dinosarus{
   update(){
     var hitFound=  Nakama.game.physics.arcade.collide(Nakama.playerGroup,Nakama.foundGroup);
     //Di chuyển lên xuống
-    if(Nakama.keyboard.isDown(this.configs.up)&&this.sprite.body.touching.down&& hitFound){
+    if(Nakama.keyboard.isDown(this.configs.up)&&this.sprite.body.touching.down && hitFound){
     //  console.log("UP button pressed");
       this.sprite.body.velocity.y = -Dinosarus.JUMP;
 
@@ -26,17 +25,32 @@ class Dinosarus{
       // this.sprite.body.velocity.y = Dinosarus.SPEED;
       this.sprite.body.gravity.y = 900;
     }
-
     //Di chuyển trái phải
     if(Nakama.keyboard.isDown(this.configs.left)){
       this.sprite.body.velocity.x = -Dinosarus.SPEED;
+      this.sprite.scale.setTo(-1,1);
       this.move();
-    }else if(Nakama.keyboard.isDown(this.configs.right)){
-      this.sprite.body.velocity.x = Dinosarus.SPEED;
+      Nakama.found.forEach(function(anyFound){
+        anyFound.stop();
+      })
+    }
+    else if(Nakama.keyboard.isDown(this.configs.right)){ //Nếu di chuyển xang bên phải
+      if(this.sprite.position.x > 1023){ // Mà đi quá giữa map
+        this.sprite.body.velocity.x = 0;
+        Nakama.found.forEach(function(anyFound){
+          anyFound.move();
+        })
+      }else {
+        this.sprite.body.velocity.x = Dinosarus.SPEED;
+      }
+      this.sprite.scale.setTo(1,1);
       this.move();
     }else {
       this.sprite.body.velocity.x = 0;
       this.stop();
+      Nakama.found.forEach(function(anyFound){
+        anyFound.stop();
+      })
     }
   }
   move(){
