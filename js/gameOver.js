0ef8b27f85653cde67b749df790facb57e02b811
PeaceQWE <peacemaker123bn@gmail.com>
2017-04-02 02:43:04 +0700

    gameover

--- js/gameOver.js
+++ js/gameOver.js
@@ -4,12 +4,22 @@ var gameOverState = {
 
     console.log("asdasfc");
     map = Nakama.game.add.sprite(0,0,'background');
-    var playButton = this.game.add.button(1000,500,"button",this.playTheGame,this);
-		    playButton.anchor.setTo(0.5,0.5);
-    var winLabel = this.game.add.sprite(850,1000,"gameOver");
+    var playButtonCheckPoint = this.game.add.button(900,800,"button",this.playTheGameAtCheckPoint,this);
+		    playButtonCheckPoint.anchor.setTo(0.5,0.5);
+        playButtonCheckPoint.scale = new Phaser.Point(0.3,0.3)
+    var playButtonNewGame = this.game.add.button(1100,800,"button",this.playTheGameAtNew,this);
+    	  playButtonNewGame.anchor.setTo(0.5,0.5);
+        playButtonNewGame.scale = new Phaser.Point(0.3,0.3)
+    var winLabel = this.game.add.sprite(1023,500,"gameOver");
+        winLabel.anchor.setTo(0.5,0.5);
 
   },
-  playTheGame: function(){
-    this.game.state.start("play");
-	}
+  playTheGameAtNew: function(){
+    again = false;
+    this.game.state.start("play",true,false,again);
+	},
+  playTheGameAtCheckPoint: function() {
+    again = true;
+    this.game.state.start("play",true,false,again);
+  }
 }
