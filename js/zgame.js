5dec16717df6a4d104ed05a48f5b10533eb95364
Diện Phạm <Din Phm>
2017-04-02 00:41:53 +0700

    again

---
+++ js/zgame.js
@@ -0,0 +1,28 @@
+var Nakama = {};
+Nakama.configs = {
+  GAME_WIDTH  : 2046,
+  GAME_HEIGHT : 1277,
+  MIN_WIDTH   : 800,
+  MIN_HEIGHT  : 500,
+  MAX_WIDTH   : 2046,
+  MAX_HEIGHT  : 1277,
+};
+ Nakama.game = new Phaser.Game(Nakama.configs.GAME_WIDTH,Nakama.configs.GAME_HEIGHT,Phaser.AUTO,'');
+
+Nakama.game.state.add('boot', bootState);
+Nakama.game.state.add('load', loadState);
+Nakama.game.state.add('menu', menuState);
+Nakama.game.state.add('play', playState);
+Nakama.game.state.add('gameOver', gameOverState);
+
+Nakama.game.state.start('boot');
+var gameOver = function(){
+  console.log("Em nó đã dính bẫy :'(")
+}
+function  dkm(){
+  //this.cooldown = true;
+}
+
+var gameOver = function(){
+  Nakama.playerGroup.getFirstAlive().kill();
+}
